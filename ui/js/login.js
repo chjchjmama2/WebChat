784a37ae3ebf8e902269ce8121e592e84789ce8f
Quang Trường <votruong284@gmail.com>
2022-03-19 14:47:20 +0700

    Fix loi login

--- ui/js/login.js
+++ ui/js/login.js
@@ -1,23 +1,23 @@
-function _(element){
-   
+function _(element) {
+
     return document.getElementById(element);
 }
 
 var login_button = _("login_button");
-login_button.addEventListener("click",collect_data);
+login_button.addEventListener("click", collect_data);
 
-function collect_data(e){
+function collect_data(e) {
     e.preventDefault();
     login_button.disabled = true;
     login_button.value = "Loading...Please wait...";
 
     var myform = _("myform");
     var inputs = myform.getElementsByTagName("INPUT");
-    
-    var data ={}
-    for(var i = inputs.length - 1; i >= 0 ;i--){
+
+    var data = {}
+    for (var i = inputs.length - 1; i >= 0; i--) {
         var key = inputs[i].name;
-        switch(key){
+        switch (key) {
             case "email":
                 data.email = inputs[i].value;
                 break;
@@ -26,30 +26,29 @@ function collect_data(e){
                 break;
         }
     }
-    send_data(data,"login");
+    send_data(data, "login");
 }
-function send_data(data, type){
+function send_data(data, type) {
     var xml = new XMLHttpRequest();
-    xml.onload = function(){
-        if(xml.readyState == 4 || xml.status == 200){
+    xml.onload = function () {
+        if (xml.readyState == 4 || xml.status == 200) {
             handle_result(xml.responseText);
             login_button.disabled = false;
             login_button.value = "Login";
         }
     }
-        data.data_type = type;
-        var data_string = JSON.stringify(data);
-        xml.open("POST","api.php",true);
-        xml.send(data_string);
+    data.data_type = type;
+    var data_string = JSON.stringify(data);
+    xml.open("POST", "api.php", true);
+    xml.send(data_string);
 }
 
-function handle_result(result){
+function handle_result(result) {
     var data = JSON.parse(result);
-    if(data.data_type == "info"){
+    if (data.data_type == "info") {
         window.location = "index.php";
-    }else
-    {
-        var error = _("error").
+    } else {
+        var error = _("error")
         error.innerHTML = data.message;
         error.style.display = "block";
     }
